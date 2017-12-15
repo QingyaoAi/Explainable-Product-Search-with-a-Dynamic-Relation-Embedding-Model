5af55c801ed2700c5d8ca1fb084de113b7c95bce
Qingyao Ai <aiqy@cs.umass.edu>
2017-12-15 10:59:12 -0500

    Runnable v0.9

---
+++ ProductSearch/PersonalizedEmbedding.py
@@ -0,0 +1,243 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# We disable pylint because we need python3 compatibility.
+from six.moves import xrange# pylint: disable=redefined-builtin
+from six.moves import zip	 # pylint: disable=redefined-builtin
+
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.ops import embedding_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import variable_scope
+import math
+import os
+import random
+import sys
+import time
+
+import numpy as np
+from six.moves import xrange# pylint: disable=redefined-builtin
+import tensorflow as tf
+
+																				
+def get_product_scores(model, user_idxs, query_word_idx, product_idxs = None, scope = None):
+	with variable_scope.variable_scope(scope or "embedding_graph"):										
+		# get user embedding [None, embed_size]										
+		user_vec = tf.nn.embedding_lookup(model.entity_dict['user']['embedding'], user_idxs)
+		# get query embedding [None, embed_size]
+		query_vec, query_embs = get_query_embedding(model, query_word_idx, model.entity_dict['word']['embedding'], True)
+
+		# get candidate product embedding [None, embed_size]										
+		product_vec = None
+		product_bias = None
+		if product_idxs != None:										
+			product_vec = tf.nn.embedding_lookup(model.entity_dict['product']['embedding'], product_idxs)									
+			product_bias = tf.nn.embedding_lookup(model.product_bias, product_idxs)
+		else:										
+			product_vec = model.entity_dict['product']['embedding']
+			product_bias = model.product_bias								
+												
+		print('Similarity Function : ' + model.similarity_func)										
+		
+		
+		if model.similarity_func == 'product':										
+			return tf.matmul((1.0 - model.Wu) * user_vec + model.Wu * query_vec, product_vec, transpose_b=True)
+		elif model.similarity_func == 'bias_product':
+			return tf.matmul((1.0 - model.Wu) * user_vec + model.Wu * query_vec, product_vec, transpose_b=True) + product_bias
+		else:										
+			user_vec = user_vec / tf.sqrt(tf.reduce_sum(tf.square(user_vec), 1, keep_dims=True))
+			query_vec = query_vec / tf.sqrt(tf.reduce_sum(tf.square(query_vec), 1, keep_dims=True))
+			product_vec = product_vec / tf.sqrt(tf.reduce_sum(tf.square(product_vec), 1, keep_dims=True))									
+			return tf.matmul((1.0 - model.Wu) * user_vec + model.Wu * query_vec, product_vec, transpose_b=True)
+
+def get_fs_from_words(model, word_vecs, reuse, scope=None):
+	with variable_scope.variable_scope(scope or 'f_s_abstraction',
+										 reuse=reuse):
+		# get mean word vectors
+		mean_word_vec = tf.reduce_mean(word_vecs, 1)
+		# get f(s)
+		f_W = variable_scope.get_variable("f_W", [model.embed_size, model.embed_size])
+		f_b = variable_scope.get_variable("f_b", [model.embed_size])
+		f_s = tf.tanh(tf.nn.bias_add(tf.matmul(mean_word_vec, f_W), f_b))
+		return f_s, [f_W, word_vecs]
+
+def get_addition_from_words(model, word_vecs, reuse, scope=None):
+	with variable_scope.variable_scope(scope or 'addition_abstraction',
+										 reuse=reuse):
+		# get mean word vectors
+		mean_word_vec = tf.reduce_mean(word_vecs, 1)
+		return mean_word_vec, [word_vecs]
+
+def get_RNN_from_words(model, word_vecs, reuse, scope=None):
+	with variable_scope.variable_scope(scope or 'RNN_abstraction',
+										 reuse=reuse):
+		cell = tf.contrib.rnn.GRUCell(model.embed_size)
+		encoder_outputs, encoder_state = tf.nn.static_rnn(cell, tf.unstack(word_vecs, axis=1), dtype=dtypes.float32)
+		return encoder_state, [word_vecs]
+
+def get_attention_from_words(model, word_vecs, reuse, scope=None):
+	with variable_scope.variable_scope(scope or 'attention_abstraction',
+										 reuse=reuse,
+										 initializer=tf.truncated_normal_initializer(mean=0.0, stddev=1.0, seed=None, dtype=tf.float32)):
+		# build mask
+		mask = tf.maximum(tf.cast(word_idxs, tf.float32) + 1.0, 1.0) # [batch,query_max_length]
+		# softmax weight
+		#print(word_idxs.get_shape())
+		gate_W = variable_scope.get_variable("gate_W", [model.embed_size])
+		#print(tf.reduce_sum(word_vecs * gate_W,2).get_shape())
+		word_weight = tf.exp(tf.reduce_sum(word_vecs * gate_W,2)) * mask 
+		word_weight = word_weight / tf.reduce_sum(word_weight,1,keep_dims=True) 
+		# weigted sum
+		att_word_vec = tf.reduce_sum(word_vecs * tf.expand_dims(word_weight,2),1)
+		return att_word_vec, [word_vecs]
+
+def get_query_embedding(model, word_idxs, word_emb, reuse, scope = None):
+	word_vecs = tf.nn.embedding_lookup(word_emb, word_idxs)
+	if 'mean' in model.net_struct: # mean vector
+		print('Query model: mean')
+		return get_addition_from_words(model, word_vecs, reuse, scope)
+	elif 'fs' in model.net_struct: # LSE f(s)
+		print('Query model: LSE f(s)')
+		return get_fs_from_words(model, word_vecs, reuse, scope)
+	elif 'RNN' in model.net_struct: # RNN
+		print('Query model: RNN')
+		return get_RNN_from_words(model, word_vecs, reuse, scope)
+	else:
+		print('Query model: Attention')
+		return get_attention_from_words(model, word_vecs, reuse, scope)
+
+
+def build_graph_and_loss(model, scope = None):											
+	with variable_scope.variable_scope(scope or "embedding_graph"):	
+		loss = None
+		regularization_terms = []
+		batch_size = array_ops.shape(model.user_idxs)[0]#get batch_size	
+		# user + query -> product
+		query_vec, qw_embs = get_query_embedding(model, model.query_word_idxs, model.entity_dict['word']['embedding'], None) # get query vector
+		regularization_terms.extend(qw_embs)						
+		model.product_bias = tf.Variable(tf.zeros([model.entity_dict['product']['size']]), name="product_b")
+		uqr_loss, uqr_embs = pair_search_loss(model, model.Wu, query_vec, model.user_idxs, # product prediction loss
+							model.entity_dict['user']['embedding'], model.product_idxs, 
+							model.entity_dict['product']['embedding'], model.product_bias, 
+							len(model.entity_dict['product']['vocab']), model.data_set.product_distribute) 			
+		regularization_terms.extend(uqr_embs)
+		uqr_loss = tf.Print(uqr_loss, [uqr_loss], 'this is uqr', summarize=5)
+		loss = tf.reduce_sum(uqr_loss)
+
+		# product + write -> word
+		pw_loss, pw_embs = relation_nce_loss(model, 0.5, model.product_idxs, 'product', 'word', 'word')
+		regularization_terms.extend(pw_embs)
+		pw_loss = tf.Print(pw_loss, [pw_loss], 'this is pw', summarize=5)
+		loss += pw_loss
+
+		# product + also_bought -> product
+		pab_loss, pab_embs = relation_nce_loss(model, 0.5, model.product_idxs, 'product', 'also_bought', 'product')
+		regularization_terms.extend(pab_embs)
+		pab_loss = tf.Print(pab_loss, [pab_loss], 'this is pab', summarize=5)
+		loss += pab_loss
+
+		# product + also_viewed -> product
+		pav_loss, pav_embs = relation_nce_loss(model, 0.5, model.product_idxs, 'product', 'also_viewed', 'product')
+		regularization_terms.extend(pav_embs)
+		pav_loss = tf.Print(pav_loss, [pav_loss], 'this is pav', summarize=5)
+		loss += pav_loss
+
+		# product + bought_together -> product
+		pbt_loss, pbt_embs = relation_nce_loss(model, 0.5, model.product_idxs, 'product', 'bought_together', 'product')
+		regularization_terms.extend(pbt_embs)
+		pbt_loss = tf.Print(pbt_loss, [pbt_loss], 'this is pbt', summarize=5)
+		loss += pbt_loss
+
+		# product + is_brand -> brand
+		pib_loss, pib_embs = relation_nce_loss(model, 0.5, model.product_idxs, 'product', 'brand', 'brand')
+		regularization_terms.extend(pib_embs)
+		pib_loss = tf.Print(pib_loss, [pib_loss], 'this is pib', summarize=5)
+		loss += pib_loss
+
+		# product + is_category -> categories
+		pic_loss, pic_embs = relation_nce_loss(model, 0.5, model.product_idxs, 'product', 'categories', 'categories')
+		regularization_terms.extend(pic_embs)
+		pic_loss = tf.Print(pic_loss, [pw_loss], 'this is pic', summarize=5)
+		loss += pic_loss
+
+		# L2 regularization
+		if model.L2_lambda > 0:
+			l2_loss = tf.nn.l2_loss(regularization_terms[0])
+			for i in xrange(1,len(regularization_terms)):
+				l2_loss += tf.nn.l2_loss(regularization_terms[i])
+			loss += model.L2_lambda * l2_loss
+
+		return loss / math_ops.cast(batch_size, dtypes.float32)										
+
+def relation_nce_loss(model, add_weight, example_idxs, head_entity_name, relation_name, tail_entity_name):
+	relation_vec = model.relation_dict[relation_name]['embedding']
+	example_emb = model.entity_dict[head_entity_name]['embedding']
+	label_idxs = model.relation_dict[relation_name]['idxs']
+	label_emb = model.entity_dict[tail_entity_name]['embedding']
+	label_bias = model.relation_dict[relation_name]['bias']
+	label_size = model.entity_dict[tail_entity_name]['size']
+	label_distribution = model.relation_dict[relation_name]['distribute']
+	loss, embs = pair_search_loss(model, add_weight, relation_vec, example_idxs, example_emb, label_idxs, label_emb, label_bias, label_size, label_distribution)
+	#print(loss.get_shape())
+	#print(model.relation_dict[relation_name]['weight'].get_shape())
+	return tf.reduce_sum(model.relation_dict[relation_name]['weight'] * loss), embs									
+
+def pair_search_loss(model, add_weight, relation_vec, example_idxs, example_emb, label_idxs, label_emb, label_bias, label_size, label_distribution):
+	batch_size = array_ops.shape(example_idxs)[0]#get batch_size										
+	# Nodes to compute the nce loss w/ candidate sampling.										
+	labels_matrix = tf.reshape(tf.cast(label_idxs,dtype=tf.int64),[batch_size, 1])										
+											
+	# Negative sampling.										
+	sampled_ids, _, _ = (tf.nn.fixed_unigram_candidate_sampler(										
+			true_classes=labels_matrix,								
+			num_true=1,								
+			num_sampled=model.negative_sample,								
+			unique=False,								
+			range_max=label_size,								
+			distortion=0.75,								
+			unigrams=label_distribution))								
+											
+	#get example embeddings [batch_size, embed_size]
+	example_vec = tf.nn.embedding_lookup(example_emb, example_idxs) * (1-add_weight) + relation_vec * add_weight							
+											
+	#get label embeddings and bias [batch_size, embed_size], [batch_size, 1]										
+	true_w = tf.nn.embedding_lookup(label_emb, label_idxs)										
+	true_b = tf.nn.embedding_lookup(label_bias, label_idxs)										
+											
+	#get sampled embeddings and bias [num_sampled, embed_size], [num_sampled, 1]										
+	sampled_w = tf.nn.embedding_lookup(label_emb, sampled_ids)										
+	sampled_b = tf.nn.embedding_lookup(label_bias, sampled_ids)										
+											
+	# True logits: [batch_size, 1]										
+	true_logits = tf.reduce_sum(tf.multiply(example_vec, true_w), 1) + true_b										
+											
+	# Sampled logits: [batch_size, num_sampled]										
+	# We replicate sampled noise lables for all examples in the batch										
+	# using the matmul.										
+	sampled_b_vec = tf.reshape(sampled_b, [model.negative_sample])										
+	sampled_logits = tf.matmul(example_vec, sampled_w, transpose_b=True) + sampled_b_vec										
+											
+	return nce_loss(true_logits, sampled_logits), [example_vec, true_w, sampled_w]						
+											
+def nce_loss(true_logits, sampled_logits):											
+	"Build the graph for the NCE loss."										
+											
+	# cross-entropy(logits, labels)										
+	true_xent = tf.nn.sigmoid_cross_entropy_with_logits(										
+			logits=true_logits, labels=tf.ones_like(true_logits))								
+	sampled_xent = tf.nn.sigmoid_cross_entropy_with_logits(										
+			logits=sampled_logits, labels=tf.zeros_like(sampled_logits))								
+	#print(true_xent.get_shape())
+	#print(sampled_xent.get_shape())
+	# NCE-loss is the sum of the true and noise (sampled words)										
+	# contributions, averaged over the batch.										
+	nce_loss_tensor = (true_xent + tf.reduce_sum(sampled_xent, 1)) 										
+	return nce_loss_tensor										
+
+
+
---
+++ ProductSearch/ProductSearchEmbedding.py
@@ -0,0 +1,355 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# We disable pylint because we need python3 compatibility.
+from six.moves import xrange# pylint: disable=redefined-builtin
+from six.moves import zip	 # pylint: disable=redefined-builtin
+
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.ops import embedding_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import variable_scope
+import math
+import os
+import random
+import sys
+import time
+
+import numpy as np
+from six.moves import xrange# pylint: disable=redefined-builtin
+import tensorflow as tf
+import PersonalizedEmbedding
+
+
+class ProductSearchEmbedding_model(object):
+	def __init__(self, data_set, window_size,
+				 embed_size, max_gradient_norm, batch_size, learning_rate, L2_lambda, query_weight,
+				 net_struct, similarity_func, forward_only=False, negative_sample = 5):
+		"""Create the model.
+	
+		Args:
+			vocab_size: the number of words in the corpus.
+			dm_feature_len: the length of document model features (query based).
+			review_size: the number of reviews in the corpus.
+			user_size: the number of users in the corpus.
+			product_size: the number of products in the corpus.
+			embed_size: the size of each embedding
+			window_size: the size of half context window
+			vocab_distribute: the distribution for words, used for negative sampling
+			review_distribute: the distribution for reviews, used for negative sampling
+			product_distribute: the distribution for products, used for negative sampling
+			max_gradient_norm: gradients will be clipped to maximally this norm.
+			batch_size: the size of the batches used during training;
+			the model construction is not independent of batch_size, so it cannot be
+			changed after initialization.
+			learning_rate: learning rate to start with.
+			learning_rate_decay_factor: decay learning rate by this much when needed.
+			forward_only: if set, we do not construct the backward pass in the model.
+			negative_sample: the number of negative_samples for training
+		"""
+		self.data_set = data_set
+		self.negative_sample = negative_sample
+		self.embed_size = embed_size
+		self.window_size = window_size
+		self.max_gradient_norm = max_gradient_norm
+		self.batch_size = batch_size * (self.negative_sample + 1)
+		self.init_learning_rate = learning_rate
+		self.L2_lambda = L2_lambda
+		self.net_struct = net_struct
+		self.similarity_func = similarity_func
+		self.global_step = tf.Variable(0, trainable=False)
+		if query_weight >= 0:
+			self.Wu = tf.Variable(query_weight, name="user_weight", dtype=tf.float32, trainable=False)
+		else:
+			self.Wu = tf.sigmoid(tf.Variable(0, name="user_weight", dtype=tf.float32))
+		self.query_max_length = data_set.query_max_length
+		self.query_word_idxs = tf.placeholder(tf.int64, shape=[None, self.query_max_length], name="query_word_idxs")
+		self.learning_rate = tf.placeholder(tf.float32, name="learning_rate")
+		init_width = 0.5 / self.embed_size
+
+		def entity(name, vocab):
+			print('%s size %s' % (name,str(len(vocab))))
+			return {
+				'name' : name,
+				'vocab' : vocab,
+				'size' : len(vocab),
+				'embedding' :tf.Variable( tf.random_uniform(									
+							[len(vocab), self.embed_size], -init_width, init_width),				
+							name="%s_emb"%name)	
+			}
+		self.entity_dict = {
+			'user' : entity('user', data_set.user_ids),
+			'product' : entity('product', data_set.product_ids),
+			'word' : entity('word', data_set.words),
+			'brand' : entity('brand', data_set.brand_ids),
+			'categories' : entity('categories', data_set.category_ids),
+		}
+
+		self.user_idxs = tf.placeholder(tf.int64, shape=[None], name="user_idxs")
+		self.product_idxs = tf.placeholder(tf.int64, shape=[None], name="product_idxs")
+		def relation(name, distribute):
+			print('%s size %s' % (name, str(len(distribute))))
+			return {
+				'distribute' : distribute,
+				'idxs' : tf.placeholder(tf.int64, shape=[None], name="%s_idxs"%name),
+				'weight' : 	tf.placeholder(tf.float32, shape=[None], name="%s_weight"%name),
+				'embedding' : tf.Variable( tf.random_uniform(									
+								[self.embed_size], -init_width, init_width),				
+								name="%s_emb"%name),
+				'bias' : tf.Variable(tf.zeros([len(distribute)]), name="%s_b"%name)	
+			}
+		self.relation_dict = {
+			'word' : relation('write', data_set.vocab_distribute),
+			'also_bought' : relation('also_bought', data_set.product_distribute),
+			'also_viewed' : relation('also_viewed', data_set.product_distribute),
+			'bought_together' : relation('bought_together', data_set.product_distribute),
+			'brand' : relation('is_brand', data_set.knowledge['brand']['distribute']),
+			'categories' : relation('is_category', data_set.knowledge['categories']['distribute'])
+		}
+
+		print('L2 lambda ' + str(self.L2_lambda))
+
+		# Training losses.
+		self.loss = PersonalizedEmbedding.build_graph_and_loss(self)
+
+		# Gradients and SGD update operation for training the model.
+		params = tf.trainable_variables()
+		if not forward_only:
+			opt = tf.train.GradientDescentOptimizer(self.learning_rate)
+			self.gradients = tf.gradients(self.loss, params)
+			
+			self.clipped_gradients, self.norm = tf.clip_by_global_norm(self.gradients,
+																	 self.max_gradient_norm)
+			self.updates = opt.apply_gradients(zip(self.clipped_gradients, params),
+											 global_step=self.global_step)
+			
+			#self.updates = opt.apply_gradients(zip(self.gradients, params),
+			#								 global_step=self.global_step)
+		else:
+			self.product_scores = PersonalizedEmbedding.get_product_scores(self, self.user_idxs, self.query_word_idxs)
+	
+		self.saver = tf.train.Saver(tf.global_variables())
+	
+	def step(self, session, input_feed, forward_only, test_mode = 'product_scores'):
+		"""Run a step of the model feeding the given inputs.
+	
+		Args:
+			session: tensorflow session to use.
+			learning_rate: the learning rate of current step
+			user_idxs: A numpy [1] float vector.
+			product_idxs: A numpy [1] float vector.
+			review_idxs: A numpy [1] float vector.
+			word_idxs: A numpy [None] float vector.
+			context_idxs: list of numpy [None] float vectors.
+			forward_only: whether to do the update step or only forward.
+	
+		Returns:
+			A triple consisting of gradient norm (or None if we did not do backward),
+			average perplexity, and the outputs.
+	
+		Raises:
+			ValueError: if length of encoder_inputs, decoder_inputs, or
+			target_weights disagrees with bucket size for the specified bucket_id.
+		"""
+	
+		# Output feed: depends on whether we do a backward step or not.
+		if not forward_only:
+			output_feed = [self.updates,	# Update Op that does SGD.
+						 #self.norm,	# Gradient norm.
+						 self.loss]	# Loss for this batch.
+		else:
+			if test_mode == 'output_embedding':
+				self.embed_output_keys = []
+				output_feed = []
+				for key in self.entity_dict:
+					self.embed_output_keys.append(key)
+					output_feed.append(self.entity_dict[key]['embedding'])
+				for key in self.relation_dict:
+					self.embed_output_keys.append(key + '_embed')
+					output_feed.append(self.relation_dict[key]['embedding'])
+				for key in self.relation_dict:
+					self.embed_output_keys.append(key + '_bias')
+					output_feed.append(self.relation_dict[key]['bias'])
+				self.embed_output_keys.append('Wu')
+				output_feed.append(self.Wu)
+				
+			else:
+				output_feed = [self.product_scores] #negative instance output
+	
+		outputs = session.run(output_feed, input_feed)
+		if not forward_only:
+			return outputs[1], None	# loss, no outputs, Gradient norm.
+		else:
+			if test_mode == 'output_embedding':
+				return outputs, self.embed_output_keys
+			else:
+				return outputs[0], None	# product scores to input user
+
+	def setup_data_set(self, data_set, words_to_train):
+		self.data_set = data_set
+		self.words_to_train = words_to_train
+		self.finished_word_num = 0
+		#if self.net_struct == 'hdc':
+		#	self.need_context = True
+
+	def intialize_epoch(self, training_seq):
+		self.train_seq = training_seq
+		self.review_size = len(self.train_seq)
+		self.cur_review_i = 0
+		self.cur_word_i = 0
+
+	def get_train_batch(self):
+		user_idxs, product_idxs, review_idxs, word_idxs, context_word_idxs = [],[],[],[],[]
+		knowledge_idxs_dict = {
+			'also_bought' : [],
+			'also_viewed' : [],
+			'bought_together' : [],
+			'brand' : [],
+			'categories' : []
+		}
+		knowledge_weight_dict = {
+			'also_bought' : [],
+			'also_viewed' : [],
+			'bought_together' : [],
+			'brand' : [],
+			'categories' : []
+		}
+		query_word_idxs = []
+		learning_rate = self.init_learning_rate * max(0.0001, 
+									1.0 - self.finished_word_num / self.words_to_train)
+		review_idx, user_idx, product_idx, query_idx = None, None, None, None
+		text_list, text_length, product_knowledge = None, None, None
+
+		# Start from a new review
+		review_idx = self.train_seq[self.cur_review_i]
+		user_idx = self.data_set.review_info[review_idx][0]
+		product_idx = self.data_set.review_info[review_idx][1]
+		query_idx = random.choice(self.data_set.product_query_idx[product_idx])
+		text_list = self.data_set.review_text[review_idx]
+		text_length = len(text_list)
+		# add knowledge
+		product_knowledge = {key : self.data_set.knowledge[key]['data'][product_idx] for key in knowledge_idxs_dict}
+		
+		while len(word_idxs) < self.batch_size:
+			#print('review %d word %d word_idx %d' % (review_idx, self.cur_word_i, text_list[self.cur_word_i]))
+			#if sample this word
+			if self.data_set.sub_sampling_rate == None or random.random() < self.data_set.sub_sampling_rate[text_list[self.cur_word_i]]:
+				user_idxs.append(user_idx)
+				product_idxs.append(product_idx)
+				query_word_idxs.append(self.data_set.query_words[query_idx])
+				review_idxs.append(review_idx)
+				word_idxs.append(text_list[self.cur_word_i])
+				# Add knowledge
+				for key in product_knowledge:
+					if len(product_knowledge[key]) < 1:
+						knowledge_idxs_dict[key].append(-1)
+						knowledge_weight_dict[key].append(0.0)
+					else:
+						knowledge_idxs_dict[key].append(random.choice(product_knowledge[key]))
+						knowledge_weight_dict[key].append(1.0)
+
+			#move to the next
+			self.cur_word_i += 1
+			self.finished_word_num += 1
+			if self.cur_word_i == text_length:
+				self.cur_review_i += 1
+				if self.cur_review_i == self.review_size:
+					break
+				self.cur_word_i = 0
+				# Start from a new review
+				review_idx = self.train_seq[self.cur_review_i]
+				user_idx = self.data_set.review_info[review_idx][0]
+				product_idx = self.data_set.review_info[review_idx][1]
+				query_idx = random.choice(self.data_set.product_query_idx[product_idx])
+				text_list = self.data_set.review_text[review_idx]
+				text_length = len(text_list)
+				# add knowledge
+				product_knowledge = {key : self.data_set.knowledge[key]['data'][product_idx] for key in knowledge_idxs_dict}
+
+
+		# Input feed: encoder inputs, decoder inputs, target_weights, as provided.
+		input_feed = {}
+		input_feed[self.learning_rate.name] = learning_rate
+		input_feed[self.user_idxs.name] = user_idxs
+		input_feed[self.product_idxs.name] = product_idxs
+		input_feed[self.query_word_idxs.name] = query_word_idxs
+		input_feed[self.relation_dict['word']['idxs'].name] = word_idxs
+		input_feed[self.relation_dict['word']['weight'].name] = [1.0 for _ in xrange(len(word_idxs))]
+		for key in knowledge_idxs_dict:
+			input_feed[self.relation_dict[key]['idxs'].name] = knowledge_idxs_dict[key]
+			input_feed[self.relation_dict[key]['weight'].name] = knowledge_weight_dict[key]
+
+		has_next = False if self.cur_review_i == self.review_size else True
+		return input_feed, has_next
+
+	def prepare_test_epoch(self):
+		self.test_user_query_set = set()
+		self.test_seq = []
+		for review_idx in xrange(len(self.data_set.review_info)):
+			user_idx = self.data_set.review_info[review_idx][0]
+			product_idx = self.data_set.review_info[review_idx][1]	
+			for query_idx in self.data_set.product_query_idx[product_idx]:
+				if (user_idx, query_idx) not in self.test_user_query_set:
+					self.test_user_query_set.add((user_idx, query_idx))
+					self.test_seq.append((user_idx, product_idx, query_idx, review_idx))
+		self.cur_uqr_i = 0
+
+	def get_test_batch(self):
+		user_idxs, product_idxs, review_idxs, word_idxs, context_word_idxs = [],[],[],[],[]
+		knowledge_idxs_dict = {
+			'also_bought' : [],
+			'also_viewed' : [],
+			'bought_together' : [],
+			'brand' : [],
+			'categories' : []
+		}
+		knowledge_weight_dict = {
+			'also_bought' : [],
+			'also_viewed' : [],
+			'bought_together' : [],
+			'brand' : [],
+			'categories' : []
+		}
+		query_word_idxs = []
+		learning_rate = self.init_learning_rate * max(0.0001, 
+									1.0 - self.finished_word_num / self.words_to_train)
+		start_i = self.cur_uqr_i
+		user_idx, product_idx, query_idx, review_idx = self.test_seq[self.cur_uqr_i]
+
+		while len(user_idxs) < self.batch_size:
+			text_list = self.data_set.review_text[review_idx]
+			user_idxs.append(user_idx)
+			product_idxs.append(product_idx)
+			query_word_idxs.append(self.data_set.query_words[query_idx])
+			review_idxs.append(review_idx)
+			word_idxs.append(text_list[0])
+			# Add knowledge
+			for key in knowledge_idxs_dict:
+				knowledge_idxs_dict[key].append(-1)
+				knowledge_weight_dict[key].append(0.0)
+			
+			#move to the next review
+			self.cur_uqr_i += 1
+			if self.cur_uqr_i == len(self.test_seq):
+				break
+			user_idx, product_idx, query_idx, review_idx = self.test_seq[self.cur_uqr_i]
+
+		# Input feed: encoder inputs, decoder inputs, target_weights, as provided.
+		input_feed = {}
+		input_feed[self.learning_rate.name] = learning_rate
+		input_feed[self.user_idxs.name] = user_idxs
+		input_feed[self.product_idxs.name] = product_idxs
+		input_feed[self.query_word_idxs.name] = query_word_idxs
+		input_feed[self.relation_dict['word']['idxs'].name] = word_idxs
+		input_feed[self.relation_dict['word']['weight'].name] = [0.0 for _ in xrange(len(word_idxs))]
+		for key in knowledge_idxs_dict:
+			input_feed[self.relation_dict[key]['idxs'].name] = knowledge_idxs_dict[key]
+			input_feed[self.relation_dict[key]['weight'].name] = knowledge_weight_dict[key]
+
+		has_next = False if self.cur_uqr_i == len(self.test_seq) else True
+		return input_feed, has_next, self.test_seq[start_i:self.cur_uqr_i]
