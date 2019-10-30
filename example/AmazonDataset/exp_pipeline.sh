eec6ab1f2c6cea1781282930439d06ba770bfc9c
Qingyao Ai <aiqy@cs.umass.edu>
2019-10-30 11:38:08 -0600

    Update example script to create output directory

--- example/AmazonDataset/exp_pipeline.sh
+++ example/AmazonDataset/exp_pipeline.sh
@@ -23,17 +23,17 @@ python ./utils/AmazonDataset/match_with_meta_knowledge.py ./tmp_data/min_count5/
 ## Also, we randomly sample 20% queries and make them unique in the test set.
 python utils/AmazonDataset/random_split_train_test_data.py tmp_data/min_count5/ 0.3 0.3
 
-# Sequentially split train/test
-## The last 20% purchases of each user are used as test data
-## Also, we manually sample 20% queries and make them unique in the test set.
-#python utils/AmazonDataset/sequentially_split_train_test_data.py tmp_data/min_count5/ 0.2 0.2
+# Create output directory
+if ! [ -d "./tmp/" ]; then
+  mkdir tmp
+fi
 
 # Run model
-python ProductSearch/main.py --data_dir=./tmp_data/min_count5/ --input_train_dir=./tmp_data/min_count5/random_query_split/ --subsampling_rate=0.0001 --window_size=3 --embed_size=400 --negative_sample=5 --learning_rate=0.5 --batch_size=64 --max_train_epoch=20 net_struct=fs --rank_cutoff100  --dynamic_weight=0.2 --L2_lambda=0.005 --steps_per_checkpoint=400
+python ProductSearch/main.py --train_dir=./tmp/ --data_dir=./tmp_data/min_count5/ --input_train_dir=./tmp_data/min_count5/random_query_split/ --subsampling_rate=0.0001 --window_size=3 --embed_size=400 --negative_sample=5 --learning_rate=0.5 --batch_size=64 --max_train_epoch=20 net_struct=fs --rank_cutoff100  --dynamic_weight=0.2 --L2_lambda=0.005 --steps_per_checkpoint=400
 #python ProductSearch/main.py --data_dir=./tmp_data/min_count5/ --input_train_dir=./tmp_data/min_count5/seq_query_split/ --subsampling_rate=0.0001 --window_size=3 --embed_size=400 --negative_sample=5 --learning_rate=0.5 --batch_size=64 --max_train_epoch=20 net_struct=fs --rank_cutoff100  --dynamic_weight=0.2 --L2_lambda=0.005 --steps_per_checkpoint=400
 
 # Test model
-python ProductSearch/main.py --data_dir=./tmp_data/min_count5/ --input_train_dir=./tmp_data/min_count5/random_query_split/ --subsampling_rate=0.0001 --window_size=3 --embed_size=400 --negative_sample=5 --learning_rate=0.5 --batch_size=64 --max_train_epoch=20 net_struct=fs --rank_cutoff100  --dynamic_weight=0.2 --L2_lambda=0.005 --steps_per_checkpoint=400 --decode=True
+python ProductSearch/main.py --train_dir=./tmp/ --data_dir=./tmp_data/min_count5/ --input_train_dir=./tmp_data/min_count5/random_query_split/ --subsampling_rate=0.0001 --window_size=3 --embed_size=400 --negative_sample=5 --learning_rate=0.5 --batch_size=64 --max_train_epoch=20 net_struct=fs --rank_cutoff100  --dynamic_weight=0.2 --L2_lambda=0.005 --steps_per_checkpoint=400 --decode=True
 #python ProductSearch/main.py --data_dir=./tmp_data/min_count5/ --input_train_dir=./tmp_data/min_count5/seq_query_split/ --subsampling_rate=0.0001 --window_size=3 --embed_size=400 --negative_sample=5 --learning_rate=0.5 --batch_size=64 --max_train_epoch=20 net_struct=fs --rank_cutoff100  --dynamic_weight=0.2 --L2_lambda=0.005 --steps_per_checkpoint=400 --decode=True
 
 # Download and install galago
